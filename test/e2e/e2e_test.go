644ee248a22a96c0b572b6af350477493bf13675
Bingtan Lu <bingtlu@ebay.com>
2023-02-21 23:11:35 +0800

    chore: Add e2e test
    Signed-off-by: Bingtan Lu <bingtlu@ebay.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,23 +1,63 @@
 package e2e
 
 import (
-	"flag"
-	"os"
+	"context"
 	"testing"
 
+	"github.com/h2non/gock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
 	"github.com/spf13/pflag"
+
+	"github.com/airconduct/go-probot"
+	probotmock "github.com/airconduct/go-probot/mock"
+	"github.com/airconduct/kuilei/pkg/app/github"
+	_ "github.com/airconduct/kuilei/pkg/plugins/factory"
+	"github.com/airconduct/kuilei/pkg/signals"
 )
 
-func TestMain(m *testing.M) {
+func TestPluginhelpers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	format.MaxLength = 1 << 20
+	RunSpecs(t, "Kuilei E2E Suite")
+}
+
+var (
+	githubApp probot.App[probot.GitHubClient]
+	ctx       context.Context
+	cancel    func()
+)
+
+var _ = BeforeSuite(func() {
 	flags := pflag.NewFlagSet("tess-cluster-e2e", pflag.ExitOnError)
-	flag.CommandLine.VisitAll(func(f *flag.Flag) {
-		if f.Name != "kubeconfig" {
-			flags.AddGoFlag(f)
-		}
+	// pflag.CommandLine = flags
+	b := github.New()
+	b.BindFlags(flags)
+	flags.Parse([]string{
+		"--github.hmac-token-file=testdata/hmac_token",
+		"--github.private-key-file=testdata/tls.key",
+		"--github.appid=1",
+		"--address=127.0.0.1",
+		"--port=7771",
+		"--path=/hook",
 	})
-	pflag.CommandLine = flags
-	// TODO: build app and bind flags
-	pflag.Parse()
-	// TODO: add test cases
-	os.Exit(m.Run())
+
+	var err error
+	githubApp, err = b.Build()
+	Expect(err).Should(BeNil())
+
+	ctx, cancel = context.WithCancel(signals.SetupSignalContext())
+	go func() {
+		Expect(githubApp.Run(ctx)).Should(Succeed())
+	}()
+})
+
+var _ = AfterSuite(func() {
+	cancel()
+	gock.Off()
+})
+
+func sendToGitHubApp(e probot.WebhookEvent, v interface{}) error {
+	return probotmock.Send(githubApp.(probotmock.AppMock[probot.GitHubClient]), e, v)
 }
